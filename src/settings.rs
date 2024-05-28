2816216339e42f7bc2eb860e8a86f4fc352794b2
servrcube <63092977+servrcube@users.noreply.github.com>
2024-05-28 21:12:16 +0200

    better colors

--- src/settings.rs
+++ src/settings.rs
@@ -1,5 +1,5 @@
 pub const INDICES: &[u32] = &[0, 1, 2, 0, 2, 3];
-pub const GRID: (u32, u32) = (100, 100);
+pub const GRID: (u32, u32) = (600, 600);
 // ([GRID.0, GRID.1][(GRID.0 < GRID.1) as usize]) calculates the min of GRID
 // this method is used, since the function min is not permitted during compile time
 pub const SIZE: f32 = 2000.0/([GRID.0, GRID.1][(GRID.0 < GRID.1) as usize]) as f32;
